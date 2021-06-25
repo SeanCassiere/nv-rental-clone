0361ee31bf56bc0a299736f0215937c94a5aa720
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-06-25 23:53:40 +0530

    Reservation view page with partial summary

--- client/src/pages/ReservationView/ReservationQuoteChargesSummary.tsx
+++ client/src/pages/ReservationView/ReservationQuoteChargesSummary.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Panel, Grid, Row, Col, Placeholder } from "rsuite";
+import { Panel, Grid, Row, Col, Placeholder, Button } from "rsuite";
 import { useSelector } from "react-redux";
 import { selectViewReservationState } from "../../redux/store";
 
 import styled from "styled-components";
+import { ReservationMiscViewDataFull } from "../../interfaces/reservations";
 
 const ReservationQuoteChargesSummary = () => {
+	const [showTaxMiscCharges, setShowTaxMiscCharges] = React.useState(false);
+	const [showNonTaxMiscCharges, setShowNonTaxMiscCharges] = React.useState(false);
 	const { reservation, isSearching } = useSelector(selectViewReservationState);
+
+	const handleShowTaxMiscCharges = React.useCallback(() => {
+		setShowTaxMiscCharges((t) => !t);
+	}, []);
+
+	const handleShowNonTaxMiscCharges = React.useCallback(() => {
+		setShowNonTaxMiscCharges((t) => !t);
+	}, []);
 
 	if (isSearching)
 		return (
@@ -21,8 +32,49 @@ const ReservationQuoteChargesSummary = () => {
 				<RowItem label='Base Rate' colText={reservation?.reservationview.basePrice} currency='$' />
 				<RowItem label='Promotion Discount' colText={reservation?.reservationview.totalDiscount} currency='$' />
 				<RowItem label='Final Base Rate' colText={reservation?.reservationview.basePrice} currency='$' />
-				<RowItem label='Total Miscellaneous Charges' colText={0} currency='$' />
-				<RowItem label='Total Miscellaneous Charges (No Tax)' colText={0} currency='$' />
+				<RowItem
+					label={
+						<p>
+							Total Miscellaneous Charges&nbsp;
+							<Button onClick={handleShowTaxMiscCharges} size='xs'>
+								Show More
+							</Button>
+						</p>
+					}
+					colText={0}
+					currency='$'
+					bold
+				/>
+				{showTaxMiscCharges && (
+					<Row>
+						<Col smPush={1} xs={24} sm={23} style={{ marginBottom: 10 }}>
+							<MisChargeBreakdownInSummary charges={reservation?.miscChargeList} currency='$' taxable />
+						</Col>
+					</Row>
+				)}
+
+				<RowItem
+					label={
+						<p>
+							Total Miscellaneous Charges (No Tax)&nbsp;
+							<Button onClick={handleShowNonTaxMiscCharges} size='xs'>
+								Show More
+							</Button>
+						</p>
+					}
+					colText={0}
+					currency='$'
+					bold
+				/>
+
+				{showNonTaxMiscCharges && (
+					<Row>
+						<Col smPush={1} xs={24} sm={23} style={{ marginBottom: 10 }}>
+							<MisChargeBreakdownInSummary charges={reservation?.miscChargeList} currency='$' />
+						</Col>
+					</Row>
+				)}
+
 				<RowItem label='Pre-Adjustment' colText={reservation?.reservationview.preAdjustment} currency='$' />
 				<RowItem label='Subtotal' colText={0} currency='$' bold />
 				<RowItem label='Tax Charges' colText={0} currency='$' />
@@ -39,6 +91,44 @@ const ReservationQuoteChargesSummary = () => {
 		</Panel>
 	);
 };
+
+const MisChargeBreakdownInSummary = React.memo(
+	({
+		charges,
+		taxable,
+		currency,
+	}: {
+		charges: ReservationMiscViewDataFull[] | undefined;
+		taxable?: true;
+		currency: string;
+	}) => {
+		if (!charges) return <></>;
+
+		const availableCharges = charges.filter((charge) =>
+			taxable ? charge.isTaxable === true : charge.isTaxable === false
+		);
+		return (
+			<>
+				{availableCharges.map((item) => (
+					<Row key={item.miscChargeId} style={{ marginBottom: 5 }}>
+						<Col xs={16} sm={9}>
+							{item.name}
+						</Col>
+						<Col xsHidden sm={10} style={{ textAlign: "right" }}>
+							{item.quantity !== 0 && <>{item.quantity}&nbsp;x&nbsp;</>}
+							{currency}&nbsp;
+							{item.value.toFixed(2)}&nbsp; x&nbsp;{item.unit}
+						</Col>
+						<Col sx={8} sm={5} style={{ textAlign: "right" }}>
+							{currency}
+							{item.totalValue.toFixed(2)}
+						</Col>
+					</Row>
+				))}
+			</>
+		);
+	}
+);
 
 const RowItem = React.memo(
 	({
