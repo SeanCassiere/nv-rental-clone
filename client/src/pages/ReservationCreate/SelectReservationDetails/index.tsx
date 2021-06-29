871f63a37999499e4b3e68d7dbbd6b3044fbb494
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-06-29 22:06:26 +0530

    Reservation details to redux form

--- client/src/pages/ReservationCreate/SelectReservationDetails/index.tsx
+++ client/src/pages/ReservationCreate/SelectReservationDetails/index.tsx
@@ -12,26 +12,72 @@ import {
 	DatePicker,
 	Button,
 } from "rsuite";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { AppDispatch } from "../../../redux/store";
-import { setJumpCreateResNavPosition } from "../../../redux/slices/createReservationSlice";
-
-const selectOptions = [
-	{
-		label: "Online",
-		value: "Online",
-		role: "Master",
-	},
-	{
-		label: "Phone",
-		value: "Phone",
-		role: "Master",
-	},
-];
+import {
+	AppDispatch,
+	selectAppConfigState,
+	selectAppKeyValuesState,
+	selectCreateReservationState,
+} from "../../../redux/store";
+import {
+	setCreateResCheckInDate,
+	setCreateResCheckInLocationId,
+	setCreateResCheckOutDate,
+	setCreateResCheckOutLocationId,
+	setCreateResTypeId,
+	setJumpCreateResNavPosition,
+} from "../../../redux/slices/createReservationSlice";
 
 const SelectReservationDetails = () => {
 	const dispatch = useDispatch<AppDispatch>();
+	const {
+		availableLocations,
+		userForm: { checkoutLocationId, checkinLocationId, reservationTypeId, checkoutDate, checkinDate },
+	} = useSelector(selectCreateReservationState);
+
+	const {
+		reservationValues: { reservationTypes },
+	} = useSelector(selectAppKeyValuesState);
+
+	const {
+		dates: { dateTimeLong },
+	} = useSelector(selectAppConfigState);
+
+	const [locations, setLocations] = React.useState<{ value: number; label: string }[]>([]);
+	const [resTypes, setResTypes] = React.useState<{ value: number; label: string }[]>([]);
+
+	// Formatting the available locations
+	React.useEffect(() => {
+		if (availableLocations === []) return;
+
+		const data = availableLocations.map((item) => {
+			return { value: item.locationId, label: item.locationName };
+		});
+
+		setLocations(data);
+	}, [availableLocations]);
+
+	// Formatting the reservation types
+	React.useEffect(() => {
+		if (reservationTypes === []) return;
+
+		const types = reservationTypes
+			.filter((i) => i.isDeleted !== true)
+			.map((item) => {
+				return { value: item.typeId, label: item.typeName };
+			});
+
+		setResTypes(types);
+	}, [reservationTypes]);
+
+	React.useEffect(() => {
+		if (checkoutDate !== "" || checkinDate !== "") return;
+		const today = new Date();
+		const tomorrow = new Date(Date.now() + 3600 * 1000 * 24);
+		dispatch(setCreateResCheckOutDate(today.toISOString()));
+		dispatch(setCreateResCheckInDate(tomorrow.toISOString()));
+	}, [checkoutDate, checkinDate, dispatch]);
 
 	const handleNextPage = React.useCallback(() => dispatch(setJumpCreateResNavPosition("customer")), [dispatch]);
 
@@ -48,11 +94,13 @@ const SelectReservationDetails = () => {
 									</Col>
 									<Col xs={12}>
 										<SelectPicker
-											data={selectOptions}
+											data={resTypes}
 											size='sm'
 											searchable={false}
 											cleanable={false}
-											style={{ width: "100%" }}
+											onSelect={(id) => dispatch(setCreateResTypeId(id))}
+											value={reservationTypeId}
+											block
 										/>
 									</Col>
 								</FormGroup>
@@ -62,13 +110,29 @@ const SelectReservationDetails = () => {
 									<Col xs={12}>
 										<FormGroup>
 											<ControlLabel>Check-Out Date</ControlLabel>
-											<DatePicker format='YYYY-MM-DD HH:mm A' showMeridian size='sm' block cleanable={false} />
+											<DatePicker
+												format={dateTimeLong}
+												showMeridian
+												defaultValue={new Date()}
+												size='sm'
+												block
+												cleanable={false}
+												onChange={(date) => dispatch(setCreateResCheckOutDate(date.toISOString()))}
+											/>
 										</FormGroup>
 									</Col>
 									<Col xs={12}>
 										<FormGroup>
 											<ControlLabel>Location</ControlLabel>
-											<SelectPicker data={selectOptions} size='sm' cleanable={false} block />
+											<SelectPicker
+												data={locations}
+												size='sm'
+												searchable={false}
+												cleanable={false}
+												value={checkoutLocationId}
+												onSelect={(e) => dispatch(setCreateResCheckOutLocationId(e))}
+												block
+											/>
 										</FormGroup>
 									</Col>
 								</Row>
@@ -78,13 +142,29 @@ const SelectReservationDetails = () => {
 									<Col xs={12}>
 										<FormGroup>
 											<ControlLabel>Check-In Date</ControlLabel>
-											<DatePicker format='YYYY-MM-DD HH:mm A' showMeridian size='sm' block cleanable={false} />
+											<DatePicker
+												format={dateTimeLong}
+												showMeridian
+												size='sm'
+												block
+												value={new Date(Date.now() + 3600 * 1000 * 24)}
+												cleanable={false}
+												onChange={(date) => dispatch(setCreateResCheckInDate(date.toISOString()))}
+											/>
 										</FormGroup>
 									</Col>
 									<Col xs={12}>
 										<FormGroup>
 											<ControlLabel>Location</ControlLabel>
-											<SelectPicker data={selectOptions} size='sm' cleanable={false} block />
+											<SelectPicker
+												data={locations}
+												size='sm'
+												searchable={false}
+												cleanable={false}
+												defaultValue={checkinLocationId}
+												onSelect={(e) => dispatch(setCreateResCheckInLocationId(e))}
+												block
+											/>
 										</FormGroup>
 									</Col>
 								</Row>
--- client/src/redux/slices/createReservationSlice.ts
+++ client/src/redux/slices/createReservationSlice.ts
@@ -1,11 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AvailableLocation } from "../../interfaces/locations/location";
+import { fakeActiveLocations } from "../../utils/fakeData2";
 
 interface SearchAgreementsSliceState {
 	currentNavPosition: number;
+	userForm: {
+		checkoutLocationId: number | null;
+		checkinLocationId: number | null;
+		reservationTypeId: number | null;
+		checkoutDate: string;
+		checkinDate: string;
+	};
+	availableLocations: AvailableLocation[];
 }
 
 const initialCreateReservationStateData: SearchAgreementsSliceState = {
 	currentNavPosition: 0,
+	userForm: {
+		checkoutLocationId: null,
+		checkinLocationId: null,
+		reservationTypeId: null,
+		checkoutDate: "",
+		checkinDate: "",
+	},
+	availableLocations: fakeActiveLocations,
 };
 
 type NavPositions = "detail" | "customer" | "vehicle" | "misCharge";
@@ -15,7 +33,7 @@ export const CreateReservationSlice = createSlice({
 	initialState: initialCreateReservationStateData,
 	reducers: {
 		clearCreateReservationState: (state) => {
-			state.currentNavPosition = 0;
+			state = initialCreateReservationStateData;
 		},
 		setJumpCreateResNavPosition: (state, action: PayloadAction<NavPositions>) => {
 			if (action.payload === "detail") state.currentNavPosition = 0;
@@ -23,10 +41,33 @@ export const CreateReservationSlice = createSlice({
 			if (action.payload === "vehicle") state.currentNavPosition = 2;
 			if (action.payload === "misCharge") state.currentNavPosition = 3;
 		},
+		setCreateResCheckOutLocationId: (state, action: PayloadAction<number>) => {
+			state.userForm.checkoutLocationId = action.payload;
+		},
+		setCreateResCheckInLocationId: (state, action: PayloadAction<number>) => {
+			state.userForm.checkinLocationId = action.payload;
+		},
+		setCreateResCheckOutDate: (state, action: PayloadAction<string>) => {
+			state.userForm.checkoutDate = action.payload;
+		},
+		setCreateResCheckInDate: (state, action: PayloadAction<string>) => {
+			state.userForm.checkinDate = action.payload;
+		},
+		setCreateResTypeId: (state, action: PayloadAction<number>) => {
+			state.userForm.reservationTypeId = action.payload;
+		},
 	},
 	extraReducers: (builder) => {},
 });
 
-export const { clearCreateReservationState, setJumpCreateResNavPosition } = CreateReservationSlice.actions;
+export const {
+	clearCreateReservationState,
+	setJumpCreateResNavPosition,
+	setCreateResCheckOutLocationId,
+	setCreateResCheckInLocationId,
+	setCreateResTypeId,
+	setCreateResCheckOutDate,
+	setCreateResCheckInDate,
+} = CreateReservationSlice.actions;
 
 export default CreateReservationSlice.reducer;
