5b7aeee53a0113b2c8109212e8de9aef4dbbc714
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-12-11 11:47:10 +1300

    feat: added react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { router } from "./routes/Router";
 
+// Create a client
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
