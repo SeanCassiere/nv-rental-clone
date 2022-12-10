9d1f73e33f686f0d5047c7ea888bcc961f4ec48d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-12-10 22:10:07 +1300

    chore: reset from create-t3-app to vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-red-400">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslintPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    sourcemap: true,
+  },
+});
