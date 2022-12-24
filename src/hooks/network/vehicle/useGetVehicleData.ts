c112f29629c1454c33da1901fe51323e7d6c99ae
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-12-24 18:43:08 +1300

    feat: add fetching vehicle data

---
+++ src/hooks/network/vehicle/useGetVehicleData.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "react-oidc-context";
+import { fetchVehicleData } from "../../../api/vehicles";
+import { type VehicleDataParsed } from "../../../utils/schemas/vehicle";
+
+export function useGetVehicleData(params: {
+  vehicleId: string | number;
+  onError?: (err: unknown) => void;
+}) {
+  const auth = useAuth();
+  const query = useQuery<VehicleDataParsed>({
+    queryKey: ["vehicleView", params.vehicleId, "data"],
+    queryFn: async () =>
+      fetchVehicleData({
+        vehicleId: params.vehicleId,
+        clientId: auth.user?.profile.navotar_clientid || "",
+        userId: auth.user?.profile.navotar_userid || "",
+        accessToken: auth.user?.access_token || "",
+      }),
+    enabled: auth.isAuthenticated,
+    onError: (err) => {
+      if (params?.onError) {
+        params?.onError(err);
+      }
+    },
+    retry: 2,
+  });
+  return query;
+}
