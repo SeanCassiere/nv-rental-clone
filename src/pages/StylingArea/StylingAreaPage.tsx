7778a71c471ddc1e203b28d2c7b7dfb9432025f1
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-12-26 01:09:55 +1300

    chore: test styling page added

---
+++ src/pages/StylingArea/StylingAreaPage.tsx
@@ -0,0 +1,15 @@
+// import {} from "@headlessui/react";
+
+const StylingAreaPage = () => {
+  return (
+    <div className="h-full">
+      <section className="md:mx-28">
+        <h2 className="text-2xl">Multi-Select</h2>
+        <div></div>
+      </section>
+      {/* <hr /> */}
+    </div>
+  );
+};
+
+export default StylingAreaPage;
