bd466dadee5d160220f87aa69b112a3d708fea9a
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-01-20 23:44:34 +1300

    refactor: remove manual user profile typings

--- src/hooks/network/user/useGetUserProfile.ts
+++ src/hooks/network/user/useGetUserProfile.ts
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { useAuth } from "react-oidc-context";
 import { fetchUserProfile } from "../../../api/users";
-import type { UserProfileType } from "../../../types/User";
+import { UserProfileSchema } from "../../../utils/schemas/user";
 
 export function useGetUserProfile() {
   const auth = useAuth();
-  const query = useQuery<UserProfileType>({
+  const query = useQuery({
     queryKey: ["users", "me"],
     queryFn: () =>
       fetchUserProfile({
         clientId: auth.user?.profile.navotar_clientid || "",
         userId: auth.user?.profile.navotar_userid || "",
         accessToken: auth.user?.access_token || "",
-      }),
+      }).then((data) => UserProfileSchema.parse(data)),
     enabled: auth.isAuthenticated,
   });
   return query;
