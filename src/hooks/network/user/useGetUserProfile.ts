941f6d5f85e46aa78dcf7ead1a3565ecc19fb707
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-01-26 17:41:30 +1300

    fix: setting the i18next language based on the user profile

--- src/hooks/network/user/useGetUserProfile.ts
+++ src/hooks/network/user/useGetUserProfile.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { useAuth } from "react-oidc-context";
+import { useTranslation } from "react-i18next";
+
 import { fetchUserProfile } from "../../../api/users";
 import { userQKeys } from "../../../utils/query-key";
 import { UserProfileSchema } from "../../../utils/schemas/user";
 
 export function useGetUserProfile() {
+  const { i18n } = useTranslation();
   const auth = useAuth();
   const query = useQuery({
     queryKey: userQKeys.me(),
@@ -15,6 +18,11 @@ export function useGetUserProfile() {
         accessToken: auth.user?.access_token || "",
       }).then((data) => UserProfileSchema.parse(data)),
     enabled: auth.isAuthenticated,
+    onSuccess: (data) => {
+      if (data?.language) {
+        i18n.changeLanguage(data.language);
+      }
+    },
   });
   return query;
 }
