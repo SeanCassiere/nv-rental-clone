ae1ff93446210c0e7e3f7a2ef52d6d765c4b7212
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-01-28 14:43:57 +1300

    feat: break the agreement summary tab info into smaller tabs

--- src/components/Agreement/AgreementSummaryTab.tsx
+++ src/components/Agreement/AgreementSummaryTab.tsx
@@ -1,17 +1,32 @@
+import { useCallback, useMemo, useState } from "react";
+
 import CustomerInformation from "../PrimaryModule/ModuleInformation/CustomerInformation";
 import RentalInformation from "../PrimaryModule/ModuleInformation/RentalInformation";
 import { RentalRatesSummary } from "../PrimaryModule/ModuleSummary/RentalRatesSummary";
+import {
+  ModuleTabs,
+  type ModuleTabConfigItem,
+} from "../PrimaryModule/ModuleTabs";
 
 import { useGetAgreementData } from "../../hooks/network/agreement/useGetAgreementData";
 import { useGetClientProfile } from "../../hooks/network/client/useGetClientProfile";
 import { useGetModuleRentalRatesSummary } from "../../hooks/network/module/useGetModuleRentalRatesSummary";
 import VehicleInformation from "../PrimaryModule/ModuleInformation/VehicleInformation";
+import { getStartingIndexFromTabName } from "../../utils/moduleTabs";
 
 type AgreementSummaryTabProps = {
   agreementId: string;
 };
 
 const AgreementSummaryTab = (props: AgreementSummaryTabProps) => {
+  const canViewCustomerInformation = true;
+  const canViewRentalInformation = true;
+
+  const [currentTab, setCurrentTab] = useState("vehicle");
+  const handleTabClick = useCallback((newTab: ModuleTabConfigItem) => {
+    setCurrentTab(newTab.id);
+  }, []);
+
   const agreementData = useGetAgreementData({
     agreementId: props.agreementId,
   });
@@ -25,9 +40,86 @@ const AgreementSummaryTab = (props: AgreementSummaryTabProps) => {
 
   const clientProfile = useGetClientProfile();
 
-  const canViewCustomerInformation = true;
-  const canViewRentalInformation = true;
-  const canViewVehicleInformation = true;
+  const tabsConfig = useMemo(() => {
+    const tabs: ModuleTabConfigItem[] = [];
+
+    tabs.push({
+      id: "vehicle",
+      label: "Vehicle",
+      component: (
+        <VehicleInformation
+          mode={isCheckedIn ? "agreement-checked-in" : "agreement-checked-out"}
+          isLoading={agreementData.isLoading}
+          data={
+            agreementData.data
+              ? {
+                  vehicleId: agreementData.data?.vehicleId,
+                  vehicleNo: agreementData.data?.vehicleNo,
+                  vehicleType: agreementData.data?.vehicleType,
+                  licenseNo: agreementData.data?.licenseNo,
+                  make: agreementData.data?.vehicleMakeName,
+                  model: agreementData.data?.modelName,
+                  year: agreementData.data?.year,
+                  fuelLevelOut: agreementData.data?.fuelLevelOut,
+                  fuelLevelIn: agreementData.data?.fuelLevelIn,
+                  odometerOut: agreementData.data?.odometerOut,
+                  odometerIn: agreementData.data?.odometerIn,
+                }
+              : {}
+          }
+        />
+      ),
+    });
+
+    if (canViewRentalInformation) {
+      tabs.push({
+        id: "rental",
+        label: "Rental & rates",
+        component: (
+          <RentalInformation
+            mode="agreement"
+            data={
+              agreementData.data?.rateList && agreementData.data?.rateList[0]
+                ? {
+                    totalDays: agreementData.data?.totalDays,
+                    rateName: agreementData.data?.rateList[0]?.rateName,
+                    dailyMilesAllowed:
+                      agreementData.data?.rateList[0]?.displaydailyMilesAllowed,
+                    weeklyMilesAllowed:
+                      agreementData.data?.rateList[0]
+                        ?.displayweeklyMilesAllowed,
+                    monthlyMilesAllowed:
+                      agreementData.data?.rateList[0]
+                        ?.displaymonthlyMilesAllowed,
+                    hourlyRate: agreementData.data?.rateList[0]?.hourlyRate,
+                    halfHourlyRate:
+                      agreementData.data?.rateList[0]?.halfHourlyRate,
+                    dailyRate: agreementData.data?.rateList[0]?.dailyRate,
+                    halfDayRate: agreementData.data?.rateList[0]?.halfDayRate,
+                    weeklyRate: agreementData.data?.rateList[0]?.weeklyRate,
+                    weekendRate:
+                      agreementData.data?.rateList[0]?.weekendDayRate,
+                    monthlyRate: agreementData.data?.rateList[0]?.monthlyRate,
+                    destination: agreementData.data?.destination,
+                  }
+                : {}
+            }
+            isLoading={agreementData.isLoading || clientProfile.isLoading}
+            currency={clientProfile.data?.currency || undefined}
+          />
+        ),
+      });
+    }
+
+    return tabs;
+  }, [
+    agreementData.data,
+    agreementData.isLoading,
+    canViewRentalInformation,
+    clientProfile.data,
+    clientProfile.isLoading,
+    isCheckedIn,
+  ]);
 
   return (
     <div className="grid max-w-full grid-cols-1 gap-4 focus:ring-0 lg:grid-cols-12">
@@ -60,64 +152,12 @@ const AgreementSummaryTab = (props: AgreementSummaryTabProps) => {
             isLoading={agreementData.isLoading}
           />
         )}
-        {canViewVehicleInformation && (
-          <VehicleInformation
-            mode={
-              isCheckedIn ? "agreement-checked-in" : "agreement-checked-out"
-            }
-            isLoading={agreementData.isLoading}
-            data={
-              agreementData.data
-                ? {
-                    vehicleId: agreementData.data?.vehicleId,
-                    vehicleNo: agreementData.data?.vehicleNo,
-                    vehicleType: agreementData.data?.vehicleType,
-                    licenseNo: agreementData.data?.licenseNo,
-                    make: agreementData.data?.vehicleMakeName,
-                    model: agreementData.data?.modelName,
-                    year: agreementData.data?.year,
-                    fuelLevelOut: agreementData.data?.fuelLevelOut,
-                    fuelLevelIn: agreementData.data?.fuelLevelIn,
-                    odometerOut: agreementData.data?.odometerOut,
-                    odometerIn: agreementData.data?.odometerIn,
-                  }
-                : {}
-            }
-          />
-        )}
-        {canViewRentalInformation && (
-          <RentalInformation
-            mode="agreement"
-            data={
-              agreementData.data?.rateList && agreementData.data?.rateList[0]
-                ? {
-                    totalDays: agreementData.data?.totalDays,
-                    rateName: agreementData.data?.rateList[0]?.rateName,
-                    dailyMilesAllowed:
-                      agreementData.data?.rateList[0]?.displaydailyMilesAllowed,
-                    weeklyMilesAllowed:
-                      agreementData.data?.rateList[0]
-                        ?.displayweeklyMilesAllowed,
-                    monthlyMilesAllowed:
-                      agreementData.data?.rateList[0]
-                        ?.displaymonthlyMilesAllowed,
-                    hourlyRate: agreementData.data?.rateList[0]?.hourlyRate,
-                    halfHourlyRate:
-                      agreementData.data?.rateList[0]?.halfHourlyRate,
-                    dailyRate: agreementData.data?.rateList[0]?.dailyRate,
-                    halfDayRate: agreementData.data?.rateList[0]?.halfDayRate,
-                    weeklyRate: agreementData.data?.rateList[0]?.weeklyRate,
-                    weekendRate:
-                      agreementData.data?.rateList[0]?.weekendDayRate,
-                    monthlyRate: agreementData.data?.rateList[0]?.monthlyRate,
-                    destination: agreementData.data?.destination,
-                  }
-                : {}
-            }
-            isLoading={agreementData.isLoading || clientProfile.isLoading}
-            currency={clientProfile.data?.currency || undefined}
-          />
-        )}
+        {/*  */}
+        <ModuleTabs
+          tabConfig={tabsConfig}
+          startingIndex={getStartingIndexFromTabName(currentTab, tabsConfig)}
+          onTabClick={handleTabClick}
+        />
       </div>
       {/*  */}
       <div className="flex flex-col gap-4 lg:col-span-5">
