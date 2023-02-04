d06e1cc5b6afb75dcd604bf300784e0e6bea4bfb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-02-04 20:02:51 +1300

    feat: add Source Sans Pro font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,22 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  fontWeight: {
+    // https://fonts.google.com/specimen/Source+Sans+Pro
+    thin: "200",
+    light: "300",
+    normal: "400",
+    medium: "600",
+    semibold: "600",
+    bold: "700",
+    black: "900",
+  },
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono],
       },
     },
   },
