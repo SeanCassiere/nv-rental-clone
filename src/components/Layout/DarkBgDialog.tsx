f413211cddacda7c7d814778331a6e1e92c0cd2c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-02-19 10:59:08 +1300

    refactor: making the dark bg dialog a generic reusable layout component

---
+++ src/components/Layout/DarkBgDialog.tsx
@@ -0,0 +1,99 @@
+import { Fragment, type ReactNode } from "react";
+import { Transition, Dialog } from "@headlessui/react";
+import classNames from "classnames";
+
+import { XMarkOutline } from "../icons";
+
+interface DarkBgDialogProps {
+  title: ReactNode;
+  description?: ReactNode;
+  show: boolean;
+  onClose?: () => void;
+  setShow: (showState: boolean) => void;
+  restrictClose?: boolean;
+  children?: ReactNode;
+  sizing?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+  hideCloseBtn?: boolean;
+}
+
+const DarkBgDialog = (props: DarkBgDialogProps) => {
+  const restrictClose = props.restrictClose ?? false;
+  const sizing = props.sizing ?? "md";
+  const hideCloseBtn = props.hideCloseBtn ?? false;
+
+  const handleClose = () => {
+    if (!restrictClose) {
+      props?.onClose?.();
+    }
+  };
+
+  return (
+    <Transition show={props.show} as={Fragment}>
+      <Dialog onClose={handleClose} className="relative">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 z-10 bg-black/30" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <div className="fixed inset-0 z-30 flex items-center justify-center">
+            <Dialog.Panel className="mx-4">
+              <div
+                className={classNames(
+                  "max-w max-h-[90vh] w-full rounded bg-white p-4",
+                  {
+                    "max-w-xs": sizing === "xs",
+                    "max-w-sm": sizing === "sm",
+                    "max-w-md": sizing === "md",
+                    "max-w-lg": sizing === "lg",
+                    "max-w-xl": sizing === "xl",
+                    "max-w-2xl": sizing === "2xl",
+                  }
+                )}
+              >
+                <Dialog.Title className="flex items-center justify-between">
+                  <div className="flex select-none items-center pr-2 text-xl font-semibold leading-6 text-gray-700">
+                    {props.title}
+                  </div>
+                  {!hideCloseBtn && (
+                    <div>
+                      <button
+                        className="rounded-full p-2 text-slate-600 focus:text-slate-900"
+                        disabled={restrictClose}
+                        onClick={handleClose}
+                      >
+                        <XMarkOutline className="h-4 w-4" />
+                      </button>
+                    </div>
+                  )}
+                </Dialog.Title>
+                {props?.description && (
+                  <Dialog.Description className="mt-2 border-b border-gray-200 pb-2 text-sm text-slate-600">
+                    {props.description}
+                  </Dialog.Description>
+                )}
+                {props.children && <>{props.children}</>}
+              </div>
+            </Dialog.Panel>
+          </div>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default DarkBgDialog;
