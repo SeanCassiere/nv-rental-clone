1dfccc92ad84232b944a9a03da35b4051edce837
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-02-20 08:52:28 +1300

    feat: adding the customer information into the add rental state

--- src/components/AddRental/CommonCustomerInformation.tsx
+++ src/components/AddRental/CommonCustomerInformation.tsx
@@ -11,12 +11,34 @@ import {
 } from "../Form";
 import SelectCustomerModal from "../Dialogs/SelectCustomerModal";
 
+const REQUIRED = "Required" as const;
+
 function CommonCustomerInformationSchema() {
-  return z.object({});
+  return z.object({
+    address: z.string().min(1, REQUIRED),
+    city: z.string().min(1, REQUIRED),
+    countryId: z.number().min(1, REQUIRED),
+    customerId: z.number().min(1, REQUIRED),
+    dateOfBirth: z.string().min(1, REQUIRED).nullable(),
+    // driverType
+    email: z.string().min(1, REQUIRED),
+    firstName: z.string().min(1, REQUIRED),
+    lastName: z.string().min(1, REQUIRED),
+    licenseExpiryDate: z.string().nullable(),
+    licenseIssueDate: z.string().nullable(),
+    licenseNumber: z.string().nullable(),
+    phone: z.string().min(1, REQUIRED),
+    stateId: z.number().min(1, REQUIRED),
+    zipCode: z.string().min(1, REQUIRED),
+  });
 }
 export type CommonCustomerInformationSchemaParsed = z.infer<
   ReturnType<typeof CommonCustomerInformationSchema>
 >;
+
+const valOpts = {
+  shouldValidate: true,
+};
 
 const CommonCustomerInformation = ({
   customerInformation,
@@ -30,15 +52,28 @@ const CommonCustomerInformation = ({
   const [showCustomerPicker, setShowCustomerPicker] = useState(false);
 
   const values: CommonCustomerInformationSchemaParsed = {
-    ...customerInformation,
+    address: customerInformation?.address || "",
+    city: customerInformation?.city || "",
+    countryId: customerInformation?.countryId || 0,
+    customerId: customerInformation?.customerId || 0,
+    dateOfBirth: customerInformation?.dateOfBirth || "",
+    email: customerInformation?.email || "",
+    firstName: customerInformation?.firstName || "",
+    lastName: customerInformation?.lastName || "",
+    licenseExpiryDate: customerInformation?.licenseExpiryDate || null,
+    licenseIssueDate: customerInformation?.licenseIssueDate || null,
+    licenseNumber: customerInformation?.licenseNumber || null,
+    phone: customerInformation?.phone || "",
+    stateId: customerInformation?.stateId || 0,
+    zipCode: customerInformation?.zipCode || "",
   };
 
   const {
     handleSubmit,
     // register,
-    // formState: { errors },
-    // getValues,
-    // setValue,
+    formState: { errors },
+    getValues,
+    setValue,
   } = useForm({
     resolver: zodResolver(CommonCustomerInformationSchema()),
     defaultValues: values,
@@ -51,7 +86,28 @@ const CommonCustomerInformation = ({
         show={showCustomerPicker}
         setShow={setShowCustomerPicker}
         onSelect={(customer) => {
-          console.log("selected customer", customer);
+          setValue("address", customer.Address1 ?? "", valOpts);
+          setValue("city", customer.City ?? "", valOpts);
+          setValue("countryId", customer.CountryId ?? 0, valOpts);
+          setValue("customerId", customer.CustomerId, valOpts);
+          setValue("dateOfBirth", customer.DateOfbirth ?? null, valOpts);
+          setValue("email", customer.Email ?? "", valOpts);
+          setValue("firstName", customer.FirstName ?? "", valOpts);
+          setValue("lastName", customer.LastName ?? "", valOpts);
+          setValue(
+            "licenseExpiryDate",
+            customer.LicenseExpiryDate ?? null,
+            valOpts
+          );
+          setValue(
+            "licenseIssueDate",
+            customer.LicenseIssueDate ?? null,
+            valOpts
+          );
+          setValue("licenseNumber", customer.LicenseNumber ?? null, valOpts);
+          setValue("phone", customer.hPhone ?? "", valOpts);
+          setValue("stateId", customer.StateId ?? 0, valOpts);
+          setValue("zipCode", customer.ZipCode ?? "", valOpts);
         }}
       />
       <InformationBlockCardWithChildren
@@ -66,7 +122,7 @@ const CommonCustomerInformation = ({
               setShowCustomerPicker(true);
             }}
           >
-            Search customers
+            Search customer
           </Button>
         </div>
         <form
@@ -76,6 +132,14 @@ const CommonCustomerInformation = ({
           className="flex flex-col gap-4 p-4"
           autoComplete="off"
         >
+          <p>Data</p>
+          <pre className="text-sm">{JSON.stringify(getValues(), null, 2)}</pre>
+          {[...Object.keys(errors)].length > 0 && (
+            <>
+              <p>Errors</p>
+              <pre className="text-sm">{JSON.stringify(errors, null, 2)}</pre>
+            </>
+          )}
           <div className="grid gap-4 md:grid-cols-2">
             <div></div>
           </div>
