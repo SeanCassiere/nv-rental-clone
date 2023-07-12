47b30c0426d2c9e22c1b0a0ce8091eba37d9e0ff
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-12 15:03:40 +1200

    chore: final

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: "all",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
+  // trailingComma: "all",
+  // tabWidth: 2,
+  // semi: true,
+  // singleQuote: false,
   plugins: [require("prettier-plugin-tailwindcss")],
   tailwindConfig: "./tailwind.config.cjs",
-  tailwindFunctions: ["clsx", "cn", "cva"],
+  tailwindFunctions: ["clsx", "cn", "cva", "twMerge"],
 }
 
 module.exports = config;
