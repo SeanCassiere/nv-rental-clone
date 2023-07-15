dbca18cd99d8a72d30eba82b8d142a41acf5973a
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-15 19:57:45 +1200

    feat: working global search

--- src/components/header/command-menu.tsx
+++ src/components/header/command-menu.tsx
@@ -19,14 +19,22 @@ import {
   CommandEmpty,
   CommandGroup,
   CommandItem,
+  CommandLoading,
 } from "@/components/ui/command";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import { searchCustomersRoute } from "@/routes/customers/searchCustomers";
 import { searchFleetRoute } from "@/routes/fleet/searchFleet";
 import { searchReservationsRoute } from "@/routes/reservations/searchReservations";
 import { searchAgreementsRoute } from "@/routes/agreements/searchAgreements";
+import { viewFleetByIdRoute } from "@/routes/fleet/fleetIdPath";
+import { viewCustomerByIdRoute } from "@/routes/customers/customerIdPath";
+import { viewAgreementByIdRoute } from "@/routes/agreements/agreementIdPath";
+import { viewReservationByIdRoute } from "@/routes/reservations/reservationIdPath";
 
 import { useDebounce } from "@/hooks/internal/useDebounce";
+import { useGetGlobalSearch } from "@/hooks/network/module/useGetGlobalSearch";
+
 import { IsMacLike, cn } from "@/utils";
 
 export const CommandMenu = () => {
@@ -38,7 +46,9 @@ export const CommandMenu = () => {
   const [open, setOpen] = React.useState(false);
   const [text, setText] = React.useState("");
 
-  const debouncedText = useDebounce(text);
+  const searchTerm = useDebounce(text, 350);
+
+  const search = useGetGlobalSearch({ searchTerm });
 
   const run = React.useCallback((command: () => unknown) => {
     command();
@@ -81,20 +91,78 @@ export const CommandMenu = () => {
         />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup
+            heading={
+              search.data && search.data.length > 0
+                ? "System results"
+                : undefined
+            }
+          >
+            {search.isLoading && (
+              <CommandLoading>
+                <Skeleton className="mx-2 mt-1 h-4 rounded-sm" />
+              </CommandLoading>
+            )}
+            {[...(search.data ? search.data : [])].map((item, idx) => (
+              <CommandItem
+                key={`${item.fullDisplayText}_${idx}`}
+                value={item.fullDisplayText}
+                onSelect={() => {
+                  if (item.module === "vehicles") {
+                    run(() =>
+                      navigate({
+                        to: viewFleetByIdRoute.to,
+                        params: { vehicleId: item.referenceId },
+                        search: () => ({ tab: "summary" }),
+                      })
+                    );
+                  }
+                  if (item.module === "customers") {
+                    run(() =>
+                      navigate({
+                        to: viewCustomerByIdRoute.to,
+                        params: { customerId: item.referenceId },
+                        search: () => ({ tab: "summary" }),
+                      })
+                    );
+                  }
+                  if (item.module === "reservations") {
+                    run(() =>
+                      navigate({
+                        to: viewReservationByIdRoute.to,
+                        params: { reservationId: item.referenceId },
+                        search: () => ({ tab: "summary" }),
+                      })
+                    );
+                  }
+                  if (item.module === "agreements") {
+                    run(() =>
+                      navigate({
+                        to: viewAgreementByIdRoute.to,
+                        params: { agreementId: item.referenceId },
+                        search: () => ({ tab: "summary" }),
+                      })
+                    );
+                  }
+                }}
+              >
+                {item.module === "customers" && (
+                  <Users2Icon className="mr-2 h-4 w-4 text-primary/70" />
+                )}
+                {item.module === "vehicles" && (
+                  <CarIcon className="mr-2 h-4 w-4 text-primary/70" />
+                )}
+                {item.module === "reservations" && (
+                  <CalendarIcon className="mr-2 h-4 w-4 text-primary/70" />
+                )}
+                {item.module === "agreements" && (
+                  <FileSignatureIcon className="mr-2 h-4 w-4 text-primary/70" />
+                )}
+                {item.displayText}
+              </CommandItem>
+            ))}
+          </CommandGroup>
           <CommandGroup heading="Navigation">
-            <CommandItem
-              onSelect={() => {
-                run(() =>
-                  navigate({
-                    to: searchCustomersRoute.to,
-                    search: () => ({ page: 1, size: 10 }),
-                  })
-                );
-              }}
-            >
-              <Users2Icon className="mr-2 h-4 w-4 text-primary/70" />
-              Customers
-            </CommandItem>
             <CommandItem
               onSelect={() => {
                 run(() =>
@@ -107,6 +175,19 @@ export const CommandMenu = () => {
             >
               <CarIcon className="mr-2 h-4 w-4 text-primary/70" />
               Fleet
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                run(() =>
+                  navigate({
+                    to: searchCustomersRoute.to,
+                    search: () => ({ page: 1, size: 10 }),
+                  })
+                );
+              }}
+            >
+              <Users2Icon className="mr-2 h-4 w-4 text-primary/70" />
+              Customers
             </CommandItem>
             <CommandItem
               onSelect={() => {
