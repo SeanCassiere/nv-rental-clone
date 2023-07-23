cc98636e5201caf9b84956eeb165415597098c40
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-23 18:36:05 +1200

    feat: wrap the rental information tab in a card

--- src/components/add-rental/rental-information/index.tsx
+++ src/components/add-rental/rental-information/index.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
   AccordionContent,
 } from "@/components/ui/accordion";
+import { Card, CardContent } from "@/components/ui/card";
 
 import { DurationStage, type DurationStageProps } from "./duration-stage";
 import { VehicleStage, type VehicleStageProps } from "./vehicle-stage";
@@ -40,48 +41,52 @@ const RentalInformationTab = (props: RentalInformationTabProps) => {
   const [tab, setTab] = React.useState("duration");
 
   return (
-    <Accordion type="single" value={tab} onValueChange={setTab}>
-      <AccordionItem value="duration">
-        <AccordionTrigger>Duration details</AccordionTrigger>
-        <AccordionContent>
-          <DurationStage
-            initialData={durationStageData}
-            onCompleted={(data) => {
-              onDurationStageComplete(data);
-              setTab("vehicle");
-            }}
-            isEdit={isEdit}
-          />
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="vehicle">
-        <AccordionTrigger>Vehicle details</AccordionTrigger>
-        <AccordionContent>
-          <VehicleStage
-            rentalInformation={durationStageData}
-            vehicleInformation={vehicleStageData}
-            onCompleted={(data) => {
-              onVehicleStageComplete(data);
-              setTab("customer");
-            }}
-            isEdit={isEdit}
-          />
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="customer">
-        <AccordionTrigger>Customer details</AccordionTrigger>
-        <AccordionContent>
-          <CustomerStage
-            customerInformation={customerStageData}
-            onCompleted={(data) => {
-              onCustomerStageComplete(data);
-              onCompleted();
-            }}
-            isEdit={isEdit}
-          />
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <Card>
+      <CardContent>
+        <Accordion type="single" value={tab} onValueChange={setTab}>
+          <AccordionItem value="duration">
+            <AccordionTrigger>Duration details</AccordionTrigger>
+            <AccordionContent>
+              <DurationStage
+                initialData={durationStageData}
+                onCompleted={(data) => {
+                  onDurationStageComplete(data);
+                  setTab("vehicle");
+                }}
+                isEdit={isEdit}
+              />
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="vehicle">
+            <AccordionTrigger>Vehicle details</AccordionTrigger>
+            <AccordionContent>
+              <VehicleStage
+                rentalInformation={durationStageData}
+                vehicleInformation={vehicleStageData}
+                onCompleted={(data) => {
+                  onVehicleStageComplete(data);
+                  setTab("customer");
+                }}
+                isEdit={isEdit}
+              />
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="customer">
+            <AccordionTrigger>Customer details</AccordionTrigger>
+            <AccordionContent>
+              <CustomerStage
+                customerInformation={customerStageData}
+                onCompleted={(data) => {
+                  onCustomerStageComplete(data);
+                  onCompleted();
+                }}
+                isEdit={isEdit}
+              />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </CardContent>
+    </Card>
   );
 };
 
