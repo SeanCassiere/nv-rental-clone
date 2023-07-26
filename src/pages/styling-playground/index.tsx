39cd118206d5d29a5e20be7b9e2c7024d350deff
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-26 17:55:38 +1200

    feat: remove bg colors

--- src/pages/styling-playground/index.tsx
+++ src/pages/styling-playground/index.tsx
@@ -8,7 +8,7 @@ const StylingAreaPage: React.FC = () => {
   useDocumentTitle(titleMaker("Styling Area"));
 
   return (
-    <div className="h-full divide-y-2 divide-slate-200 overflow-y-auto bg-slate-50 px-2">
+    <div className="overflow-y-auto bg-background px-2">
       <section className="py-10 md:mx-28">
         <h2 className="text-2xl">JSURL Utils</h2>
         <JsURLDecoder />
