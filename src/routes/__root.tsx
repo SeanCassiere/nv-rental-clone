82c12c8ea4e5b21da3fc248909794317e880ee86
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-30 15:52:46 +1200

    feat: fetching client features on application load

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,41 @@ import { Outlet, RootRoute } from "@tanstack/router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { HeaderLayout } from "@/components/header/header-layout";
+
+import { queryClient as qc } from "@/app-entry";
+
 import { UI_APPLICATION_SHOW_ROUTER_DEVTOOLS } from "@/utils/constants";
+import { getAuthToken } from "@/utils/authLocal";
+import { clientQKeys } from "@/utils/query-key";
+import { fetchClientFeatures } from "@/api/clients";
 
 export const rootRoute = new RootRoute({
+  loader: async () => {
+    const auth = getAuthToken();
+
+    if (auth) {
+      const promises = [];
+
+      const featuresKey = clientQKeys.features();
+      if (!qc.getQueryData(featuresKey)) {
+        promises.push(
+          qc.prefetchQuery({
+            queryKey: featuresKey,
+            queryFn: async () =>
+              fetchClientFeatures({
+                clientId: auth.profile.navotar_clientid,
+                accessToken: auth.access_token,
+              }),
+            staleTime: 1000 * 60 * 5, // 5 minutes
+          })
+        );
+      }
+
+      await Promise.all(promises);
+    }
+
+    return {};
+  },
   component: () => {
     return (
       <HeaderLayout>
