75d361625719ba5160d37b0989228dafedca4846
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-04 22:40:45 +1200

    chore: configure tailwindcss configure plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -74,6 +74,7 @@ module.exports = {
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
     require("tailwindcss-animate"),
   ],
 };
