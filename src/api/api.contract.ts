8a7bdd5b20ff68522c576f3c893c4a1ede52c7fb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-07 22:50:09 +1200

    feat: move all client requests to ts-rest

--- src/api/api.contract.ts
+++ src/api/api.contract.ts
@@ -8,21 +8,56 @@ import {
 } from "@/api/api.helpers";
 
 import { AgreementListItemListSchema } from "@/schemas/agreement";
+import {
+  ClientFeatureListSchema,
+  ClientProfileSchema,
+  ClientScreenSettingListSchema,
+} from "@/schemas/client";
 
 const c = initContract();
 
-const contract = c.router({
-  getAgreementsList: {
-    method: "GET",
-    path: "/v3/agreements",
-    query: PaginationSchema.merge(UserAndClientIdAuthSchema).extend({
-      currentDate: z.string(),
-    }),
-    responses: {
-      200: AgreementListItemListSchema,
-      404: StructuredErrorSchema,
+const contract = c.router(
+  {
+    getAgreementsList: {
+      method: "GET",
+      path: "/v3/agreements",
+      query: PaginationSchema.merge(UserAndClientIdAuthSchema).extend({
+        currentDate: z.string(),
+      }),
+      responses: {
+        200: AgreementListItemListSchema,
+        404: StructuredErrorSchema,
+      },
+    },
+    getClientProfile: {
+      method: "GET",
+      path: "/v3/clients/:clientId",
+      responses: {
+        200: ClientProfileSchema,
+        404: StructuredErrorSchema,
+      },
+    },
+    getClientFeatures: {
+      method: "POST",
+      path: "/v3/clients/:clientId/clientfeatures",
+      body: z.object({}).optional(),
+      responses: {
+        200: ClientFeatureListSchema,
+        404: StructuredErrorSchema,
+      },
+    },
+    getClientScreenSettings: {
+      method: "GET",
+      path: "/v3/clients/:clientId/screensettings",
+      responses: {
+        200: ClientScreenSettingListSchema,
+        404: StructuredErrorSchema,
+      },
     },
   },
-});
+  {
+    strictStatusCodes: true,
+  }
+);
 
 export { contract };
