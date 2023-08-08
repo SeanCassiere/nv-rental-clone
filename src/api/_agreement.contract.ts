f93c6b5c98b1b0b9800421a8801d98f54695863f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-08 22:31:20 +1200

    feat: remove get agreement list till the time comes

--- src/api/_agreement.contract.ts
+++ src/api/_agreement.contract.ts
@@ -2,27 +2,13 @@ import { z } from "zod";
 
 import { c } from "@/api/c";
 import {
-  PaginationSchema,
+  // PaginationSchema,
   StructuredErrorSchema,
   UserAndClientIdAuthSchema,
 } from "./helpers";
-import {
-  AgreementDataSchema,
-  AgreementListItemListSchema,
-} from "@/schemas/agreement";
+import { AgreementDataSchema } from "@/schemas/agreement";
 
 const rootAgreementContract = c.router({
-  getAgreementsList: {
-    method: "GET",
-    path: "/v3/agreements",
-    query: PaginationSchema.merge(UserAndClientIdAuthSchema).extend({
-      currentDate: z.string(),
-    }),
-    responses: {
-      200: AgreementListItemListSchema,
-      404: StructuredErrorSchema,
-    },
-  },
   getAgreementById: {
     method: "GET",
     path: "/v3/agreements/:agreementId",
