a1ecce44ba9dd7bb1b8ffed7cd689824528f666d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-17 19:36:13 +1200

    feat: not returning the auth token if expired

--- src/utils/authLocal.ts
+++ src/utils/authLocal.ts
@@ -3,6 +3,7 @@ import { OIDC_AUTHORITY, OIDC_CLIENT_ID } from "./constants";
 
 const OidcSessionStorageSchema = z.object({
   access_token: z.string(),
+  expires_at: z.number(),
   profile: z.object({
     navotar_clientid: z.string(),
     navotar_userid: z.string(),
@@ -20,8 +21,17 @@ export function getAuthToken() {
 
   try {
     const data = OidcSessionStorageSchema.parse(JSON.parse(sessionItem));
+
+    if (data.expires_at * 1000 < Date.now()) {
+      throw new Error("Token expired");
+    }
+
     return data;
   } catch (error) {
+    if (error instanceof z.ZodError || error instanceof Error) {
+      console.error("getAuthToken error\n", error);
+    }
+
     return null;
   }
 }
