6be5e259b16df58b12d214592e1d760e84eeebde
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-22 19:37:36 +1200

    chore: setup the `@ianvs/prettier-plugin-sort-imports` config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,44 @@
-/** @type {import("prettier").Config} */
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
   trailingComma: "es5",
   tabWidth: 2,
   semi: true,
   singleQuote: false,
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 80,
+  plugins: [
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
   tailwindConfig: "./tailwind.config.cjs",
   tailwindFunctions: ["clsx", "cn", "cva", "twMerge"],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/components/(.*)$",
+    "",
+    "^@/hooks/(.*)$",
+    "^@/api/(.*)$",
+    "",
+    "^@/routes/(.*)$",
+    "",
+    "^@/schemas/(.*)$",
+    "",
+    "^@/utils/(.*)$",
+    "^@/types/(.*)$",
+    "",
+    "^@/(.*)$",
+    "",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
 };
 
 module.exports = config;
