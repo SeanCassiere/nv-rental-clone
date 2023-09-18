bebd033f281144cb9537cbf817e7a5eb12f637fb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-09-18 23:22:47 +1200

    feat: stuff

---
+++ src/components/ui/input-checkbox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
+function InputCheckbox(props: {
+  label: React.ReactNode;
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+}) {
+  const id = React.useId();
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox
+        id={id}
+        checked={props.checked}
+        onCheckedChange={props.onCheckedChange}
+      />
+      <label
+        htmlFor={id}
+        className="truncate text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        {props.label}
+      </label>
+    </div>
+  );
+}
+
+export { InputCheckbox };
