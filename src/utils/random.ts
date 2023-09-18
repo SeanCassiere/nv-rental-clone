db95029820ebc676a54e6cf7cf5c1a2b8b8f48d8
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-09-18 20:35:37 +1200

    feat: add wait function

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -7,3 +7,11 @@ export function b64_decode(str: string) {
   const decoded = decodeURIComponent(window.atob(str));
   return decoded;
 }
+
+export function wait(ms: number): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
