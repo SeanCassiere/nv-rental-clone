4163d7e69d7869e43a6035ad81fc40c1b0caca63
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-13 12:00:42 +1300

    feat: defaults for summary items and extracting the additional accessors

--- src/components/report/presentation/default-view.tsx
+++ src/components/report/presentation/default-view.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import type { ColumnDef } from "@tanstack/react-table";
 
 import { useReportContext } from "@/hooks/context/view-report";
 
+import type { TReportResult } from "@/schemas/report";
+
+const KNOWN_REMOVAL_ACCESSORS = [
+  "Amounts",
+  "AmountIdle",
+  "Totals",
+  "TotalsName",
+];
+
 const DefaultView = () => {
-  const { resultState: state } = useReportContext();
+  const { resultState: state, report } = useReportContext();
 
   if (state.status !== "success") {
     throw new Error(
@@ -26,20 +36,44 @@ const DefaultView = () => {
     });
 
     // should not contain the above summary items
-    const normalRows = data.filter(
-      (row) => !filteredSummaryItems.includes(row)
-    );
+    const normalRows = data
+      .filter((row) => !filteredSummaryItems.includes(row))
+      .map(
+        (
+          row // remove the known accessors from the normal row accessors
+        ) =>
+          Object.entries(row).reduce((acc, [key, value]) => {
+            if (KNOWN_REMOVAL_ACCESSORS.includes(key)) return acc;
+            acc[key] = value;
+            return acc;
+          }, {} as TReportResult)
+      );
 
+    // should only contain the above summary items mapped to the correct format
     const summaryItems = filteredSummaryItems.map((row) => ({
       value: row?.Amounts ?? row?.AmountIdle ?? 0,
       name: row?.Totals ?? row?.TotalsName ?? "-",
     }));
 
+    // extra accessors from the normal rows
+    const normalRowAccessors = Object.keys(normalRows[0] ?? {});
+    const knownAccessors = report.outputFields.map((field) => field.name);
+
+    // remove the known accessors from the normal row accessors
+    const additionalAccessors = normalRowAccessors.filter(
+      (accessor) => !knownAccessors.includes(accessor)
+    );
+
     return {
       rows: normalRows,
       summaryItems,
+      additionalAccessors,
     };
-  }, [data]);
+  }, [data, report.outputFields]);
+
+  console.log("sanitizedRows", sanitizedRows);
+
+  const columnDefs = React.useMemo(() => {}, []);
 
   return (
     <section className="mx-2 mt-4 sm:mx-4 sm:px-1">DefaultReportView</section>
