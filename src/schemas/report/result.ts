63540212605304d45e9e9685309bace53268f420
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-12 19:55:41 +1300

    feat: broadening the return type

--- src/schemas/report/result.ts
+++ src/schemas/report/result.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const ReportResultSchema = z.record(
   z.string(),
-  z.string().or(z.number()).or(z.null())
+  z.string().or(z.number()).or(z.boolean()).or(z.null())
 );
 export type TReportResult = z.infer<typeof ReportResultSchema>;
 export const ReportResultList = z.array(ReportResultSchema);
