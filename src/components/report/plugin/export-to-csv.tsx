f7db39dae09d768449219c114c0484fe899477e8
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-13 22:39:12 +1300

    feat: only process info upon user action

--- src/components/report/plugin/export-to-csv.tsx
+++ src/components/report/plugin/export-to-csv.tsx
@@ -3,23 +3,38 @@ import * as React from "react";
 import type { ReportTablePlugin } from "@/types/report";
 
 export const exportToCsv: ReportTablePlugin = ({ table }) => {
-  const columns = table
-    .getAllFlatColumns()
-    .map((col) => col.columnDef.header || "");
-  const rows = table.getRowModel().rows.map((row) =>
-    row.getVisibleCells().reduce((acc, curr) => {
-      const value = curr.getValue() as string;
-      acc.push(value);
-      return acc;
-    }, [] as string[])
-  );
-
   const onClick = () => {
-    console.log("columns", columns);
-    console.log(
-      "dummy csv\n",
-      [columns.join(","), rows.map((list) => list.join(","))].join("\n")
+    const start = new Date();
+
+    const columns = table
+      .getAllFlatColumns()
+      .map((col) => col.columnDef.header || "");
+    const rows = table.getRowModel().rows.map((row) =>
+      row.getVisibleCells().reduce((acc, info) => {
+        const cellGetter = info.column.columnDef.cell;
+        const value =
+          typeof cellGetter === "function"
+            ? cellGetter(info.getContext())
+            : info.getValue();
+        acc.push(value);
+        return acc;
+      }, [] as any[])
     );
+
+    const end = new Date();
+    const diff = end.getTime() - start.getTime();
+    const diffInSeconds = diff / 1000;
+
+    console.log("Start: Export to csv");
+    console.table([
+      ["Started at", start.toJSON()],
+      ["Ended at", end.toJSON()],
+      ["Time taken (seconds)", diffInSeconds],
+      ["Rows processed", rows.length],
+      ["Cells processed", rows.length * columns.length],
+    ]);
+    console.table([columns, ...rows]);
+    console.log("End: Export to csv");
   };
   return <button onClick={onClick}>Export to csv</button>;
 };
