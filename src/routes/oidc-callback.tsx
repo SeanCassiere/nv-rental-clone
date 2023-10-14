74e608aece7cc6b86485c9796f7efcdbd0e1d1d0
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-14 18:42:27 +1300

    fix: oidc-callback triggering multiple authorization requests (#215)

--- src/routes/oidc-callback.tsx
+++ src/routes/oidc-callback.tsx
@@ -1,10 +1,12 @@
-import { redirect, Route } from "@tanstack/react-router";
+import { Route } from "@tanstack/react-router";
 import { hasAuthParams } from "react-oidc-context";
 import { z } from "zod";
 
 import { LoadingPlaceholder } from "@/components/loading-placeholder";
 
 import { LS_OIDC_REDIRECT_URI_KEY } from "@/utils/constants";
+
+import { router } from "@/app-entry";
 
 import { rootRoute } from "./__root";
 
@@ -21,12 +23,27 @@ export const oidcCallbackRoute = new Route({
   }),
   path: "oidc-callback",
   loaderContext: ({ search }) => ({ redirectPath: search.redirect ?? null }),
-  beforeLoad: async ({ context, preload }) => {
-    if (preload) return;
-
+  loader: async ({ context, preload }) => {
+    if (preload) return {};
     const { redirectPath, auth } = context;
 
-    const isAuthParams = hasAuthParams();
+    const routerLocation = router.state.location;
+
+    const isAuthParams = hasAuthParams({
+      ancestorOrigins: window.location.ancestorOrigins,
+      host: window.location.host,
+      origin: window.location.origin,
+      hostname: window.location.hostname,
+      port: window.location.port,
+      protocol: window.location.protocol,
+      replace: window.location.replace,
+      assign: window.location.assign,
+      reload: window.location.reload,
+      pathname: routerLocation.pathname,
+      hash: routerLocation.hash,
+      href: routerLocation.href,
+      search: routerLocation.searchStr,
+    });
 
     // if there are no auth params, begin the sign-in process
     if (!isAuthParams) {
@@ -52,10 +69,9 @@ export const oidcCallbackRoute = new Route({
     );
     const searchParamsObj = Object.fromEntries(searchParams.entries());
 
-    throw redirect({
-      to: (pathname as any) ?? "/",
-      search: searchParamsObj,
-    });
+    router.navigate({ to: (pathname as any) ?? "/", search: searchParamsObj });
+
+    return {};
   },
   component: LoadingPlaceholder,
 });
