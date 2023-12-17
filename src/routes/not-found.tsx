efb8c0bc4cb2cfd7ff07f58def8cc47f3d4906c1
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-12-18 11:41:08 +1300

    feat: page content has been added to localization

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -1,10 +1,13 @@
 import { Link, Route } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
 
 import { cn } from "@/utils";
 
 import { rootRoute } from "./__root";
 
 const NotFoundPage = () => {
+  const { t } = useTranslation();
+
   return (
     <section
       className={cn(
@@ -13,20 +16,21 @@ const NotFoundPage = () => {
     >
       <div className="my-auto flex-shrink-0 sm:py-32">
         <p className="text-sm font-semibold uppercase tracking-wide text-primary">
-          404 error
+          {t("labelError", { ns: "messages", label: 404 })}
         </p>
         <h1 className="mt-2 text-2xl font-semibold tracking-tight text-foreground sm:text-4xl">
-          Page not found
+          {t("pageNotFound", { ns: "messages" })}
         </h1>
         <p className="mt-2 text-base text-foreground/80">
-          Sorry, we couldn’t find the page you’re looking for.
+          {t("sorryCouldNotFindThatPage", { ns: "messages" })}
         </p>
         <div className="mt-6">
           <Link
             to="/"
             className="text-base font-medium text-primary hover:text-primary/80"
           >
-            <span aria-hidden="true">&larr; </span>Go back to dashboard
+            <span aria-hidden="true">&larr; </span>
+            {t("goBack", { ns: "messages", context: "dashboard" })}
           </Link>
         </div>
       </div>
