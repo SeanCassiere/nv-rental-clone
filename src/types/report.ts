cd5d53e624d03247559338f0f7bdd265003d2a8b
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-01-18 23:37:15 +1300

    fix: unused type

--- src/types/report.ts
+++ src/types/report.ts
@@ -1,7 +1,5 @@
 import type { Table } from "@tanstack/react-table";
 
-import type { TReportResult } from "@/schemas/report";
-
 export type ReportFilterOption = {
   display: string;
   value: string;
