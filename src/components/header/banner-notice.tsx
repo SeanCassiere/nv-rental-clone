09f0ab60ef62bebe4888256b8a55969bd4480833
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-01-26 00:02:06 +1300

    feat: banners

--- src/components/header/banner-notice.tsx
+++ src/components/header/banner-notice.tsx
@@ -10,12 +10,12 @@ import { STORAGE_KEYS } from "@/utils/constants";
 
 const MessageText = ({ message }: { message: ServerMessage }) => (
   <>
-    <strong className="font-semibold text-foreground">
+    <span className="flex items-center font-semibold">
       {message.title}
       {message.description && (
-        <icons.Dot className="mb-1 hidden h-3 w-3 sm:mx-0.5 sm:inline sm:h-5 sm:w-5" />
+        <icons.Dot className="mx-0.5 mb-1 mt-1 hidden h-3 w-3 sm:inline sm:h-5 sm:w-5" />
       )}
-    </strong>
+    </span>
     {message.description && (
       <span className="group-hover:underline group-hover:underline-offset-4">
         {message.description}
@@ -40,24 +40,24 @@ export const BannerNotice = React.memo(
     if (hidden) return null;
 
     return (
-      <article className="mx-auto flex w-full max-w-[1700px] flex-1 items-center gap-6 pb-2.5 pl-3.5 pr-1 pt-3 md:px-16">
+      <article className="mx-auto flex w-full max-w-[1700px] flex-1 items-center gap-6 pb-2 pl-3.5 pr-1 pt-2.5 text-bannerPromo-foreground md:px-16 md:pb-1 md:pt-1.5">
         {message.link ? (
           <a
             href={message.link}
-            className="[text-wrap=balance] group flex flex-1 flex-col flex-wrap items-start text-base leading-normal text-bannerPromo-foreground sm:flex-row sm:items-center"
+            className="[text-wrap=balance] group flex flex-1 flex-col flex-wrap items-start gap-1 text-sm leading-tight sm:flex-row sm:items-center md:items-center md:gap-0"
             target={message.link.startsWith("http") ? "_blank" : "_self"}
             rel="noopener noreferrer"
           >
             <MessageText message={message} />
           </a>
         ) : (
-          <p className="flex flex-1 flex-col flex-wrap items-start text-base leading-normal text-bannerPromo-foreground sm:flex-row sm:items-center">
+          <p className="flex flex-1 flex-col flex-wrap items-start gap-1 text-sm leading-tight sm:flex-row sm:items-center md:items-center md:gap-0">
             <MessageText message={message} />
           </p>
         )}
         <div className="flex justify-end">
           <button
-            className="p-3 text-foreground hover:text-foreground/70"
+            className="p-3 text-bannerPromo-foreground hover:text-bannerPromo-foreground/70"
             onClick={onDismiss}
           >
             <icons.X className="h-4 w-4" />
