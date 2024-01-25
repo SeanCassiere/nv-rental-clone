d8d4f7d1db7bb05401929b5e688764edd6ed7c93
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-01-26 02:50:56 +1300

    feat: make the loading spinner visible using the foreground color

--- src/components/loading-placeholder.tsx
+++ src/components/loading-placeholder.tsx
@@ -5,7 +5,7 @@ import { icons } from "@/components/ui/icons";
 const LoadingPlaceholder = () => {
   return (
     <div className="grid min-h-[100dvh] place-items-center bg-background">
-      <icons.Loading className="h-24 w-24 animate-spin" />
+      <icons.Loading className="h-24 w-24 animate-spin text-foreground" />
     </div>
   );
 };
