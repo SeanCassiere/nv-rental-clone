663df4112395194fa8c5f4d0f079c1b406a51ddb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-01-30 21:17:19 +1300

    feat: string feature support

--- src/routes/-components/feature-toggles-dialog.tsx
+++ src/routes/-components/feature-toggles-dialog.tsx
@@ -9,6 +9,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { icons } from "@/components/ui/icons";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -19,7 +20,11 @@ import {
 
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 
-import { featureFlags, type DropdownFeatureFlag } from "@/utils/features";
+import {
+  featureFlags,
+  type DropdownFeatureFlag,
+  type StringFeatureFlag,
+} from "@/utils/features";
 
 import { useGlobalDialogContext } from "@/context/modals";
 
@@ -45,17 +50,13 @@ export function FeatureTogglesDialog() {
             className="min-h-96 max-w-full divide-y divide-muted overflow-x-auto"
           >
             {featureFlags.map((feature, idx) => {
+              const key = `feature_toggle_${feature.id}_${idx}`;
               if (feature.input_type === "dropdown") {
-                return (
-                  <DropdownFeatureInput
-                    key={`feature_toggle_${feature.id}_${idx}`}
-                    feature={feature}
-                  />
-                );
+                return <DropdownFeatureInput key={key} feature={feature} />;
               }
 
               if (feature.input_type === "string") {
-                return null;
+                return <StringFeatureInput key={key} feature={feature} />;
               }
 
               return null;
@@ -67,6 +68,75 @@ export function FeatureTogglesDialog() {
   );
 }
 
+// Feature toggle component for the input_type "string"
+interface StringFeatureInputProps {
+  feature: StringFeatureFlag;
+}
+
+function StringFeatureInput(props: StringFeatureInputProps) {
+  const { feature } = props;
+
+  const [value, setValue] = useLocalStorage(feature.id, feature.default_value);
+
+  const [editValue, onEditValueChange] = React.useState(value);
+
+  const handleResetToDefault = React.useCallback(() => {
+    window.localStorage.removeItem(feature.id);
+    onEditValueChange(feature.default_value);
+  }, [feature.default_value, feature.id]);
+
+  const handleSave = React.useCallback(() => {
+    if (editValue === feature.default_value) {
+      handleResetToDefault();
+      return;
+    }
+    setValue(editValue);
+  }, [editValue, feature.default_value, handleResetToDefault, setValue]);
+
+  return (
+    <li className="flex max-w-full flex-col items-center justify-between gap-x-6 gap-y-2 py-2 md:flex-row">
+      <div className="flex min-w-0 flex-col text-sm">
+        <p className="font-semibold leading-6 text-foreground">
+          {feature.name}
+        </p>
+        <p className="mt-1 leading-5 text-muted-foreground">
+          {feature.description}
+        </p>
+      </div>
+      <div className="flex items-center justify-center gap-2 md:min-w-32 md:grow-0 md:flex-col md:pr-1">
+        <Input
+          className="w-full md:h-8"
+          value={editValue}
+          onChange={(evt) => onEditValueChange(evt.target.value)}
+        />
+        <div className="flex gap-2">
+          <Button
+            type="button"
+            size="icon"
+            variant="ghost"
+            className="md:h-7 md:w-7"
+            onClick={handleResetToDefault}
+          >
+            <icons.RotateBackwards className="h-3.5 w-3.5" />
+            <span className="sr-only">Reset</span>
+          </Button>
+          <Button
+            type="button"
+            size="icon"
+            variant="outline"
+            className="md:h-7 md:w-7"
+            onClick={handleSave}
+          >
+            <icons.Save className="h-3.5 w-3.5" />
+            <span className="sr-only">Save</span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+// Feature toggle component for the input_type "dropdown"
 interface DropdownFeatureInputProps {
   feature: DropdownFeatureFlag;
 }
