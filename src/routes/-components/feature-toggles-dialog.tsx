de07e6029ed0cce2c3f9773a77a7a3d33ee0d798
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-01-30 21:40:02 +1300

    feat: add localization

--- src/routes/-components/feature-toggles-dialog.tsx
+++ src/routes/-components/feature-toggles-dialog.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
 
 import { EmptyState } from "@/components/layouts/empty-state";
@@ -77,7 +78,7 @@ export function FeatureTogglesDialog() {
   );
 }
 
-const QUICK_DISMISS_TOAST_OPTIONS = {
+const TOAST_DISMISS_OPTIONS = {
   dismissible: false,
   closeButton: false,
   duration: 1500,
@@ -90,6 +91,7 @@ interface StringFeatureInputProps {
 
 function StringFeatureInput(props: StringFeatureInputProps) {
   const { feature } = props;
+  const { t } = useTranslation();
 
   const [value, setValue] = useLocalStorage(feature.id, feature.default_value);
 
@@ -99,10 +101,10 @@ function StringFeatureInput(props: StringFeatureInputProps) {
     window.localStorage.removeItem(feature.id);
     onEditValueChange(feature.default_value);
     toast.info(
-      `${feature.name} feature reset to default`,
-      QUICK_DISMISS_TOAST_OPTIONS
+      t("labelFeatureReset", { ns: "messages", label: feature.name }),
+      TOAST_DISMISS_OPTIONS
     );
-  }, [feature.default_value, feature.id, feature.name]);
+  }, [feature.default_value, feature.id, feature.name, t]);
 
   const handleSave = React.useCallback(() => {
     if (editValue === feature.default_value) {
@@ -111,8 +113,11 @@ function StringFeatureInput(props: StringFeatureInputProps) {
     } else {
       setValue(editValue);
     }
-    toast.info(`${feature.name} feature updated`, QUICK_DISMISS_TOAST_OPTIONS);
-  }, [editValue, feature.default_value, feature.id, feature.name, setValue]);
+    toast.info(
+      t("labelFeatureUpdated", { ns: "messages", label: feature.name }),
+      TOAST_DISMISS_OPTIONS
+    );
+  }, [editValue, feature.default_value, feature.id, feature.name, setValue, t]);
 
   return (
     <li className="flex max-w-full flex-col items-center justify-between gap-x-6 gap-y-2 py-2 md:flex-row">
@@ -139,7 +144,9 @@ function StringFeatureInput(props: StringFeatureInputProps) {
             onClick={handleResetToDefault}
           >
             <icons.RotateBackwards className="h-3.5 w-3.5" />
-            <span className="sr-only">Reset</span>
+            <span className="sr-only">
+              {t("buttons.reset", { ns: "labels" })}
+            </span>
           </Button>
           <Button
             type="button"
@@ -149,7 +156,9 @@ function StringFeatureInput(props: StringFeatureInputProps) {
             onClick={handleSave}
           >
             <icons.Save className="h-3.5 w-3.5" />
-            <span className="sr-only">Save</span>
+            <span className="sr-only">
+              {t("buttons.save", { ns: "labels" })}
+            </span>
           </Button>
         </div>
       </div>
@@ -164,6 +173,7 @@ interface DropdownFeatureInputProps {
 
 function DropdownFeatureInput(props: DropdownFeatureInputProps) {
   const { feature } = props;
+  const { t } = useTranslation();
 
   const [value, setValue] = useLocalStorage(feature.id, feature.default_value);
 
@@ -173,10 +183,10 @@ function DropdownFeatureInput(props: DropdownFeatureInputProps) {
     window.localStorage.removeItem(feature.id);
     onEditValueChange(feature.default_value);
     toast.info(
-      `${feature.name} feature reset to default`,
-      QUICK_DISMISS_TOAST_OPTIONS
+      t("labelFeatureReset", { ns: "messages", label: feature.name }),
+      TOAST_DISMISS_OPTIONS
     );
-  }, [feature.default_value, feature.id, feature.name]);
+  }, [feature.default_value, feature.id, feature.name, t]);
 
   const handleSave = React.useCallback(() => {
     if (editValue === feature.default_value) {
@@ -185,8 +195,11 @@ function DropdownFeatureInput(props: DropdownFeatureInputProps) {
     } else {
       setValue(editValue);
     }
-    toast.info(`${feature.name} feature updated`, QUICK_DISMISS_TOAST_OPTIONS);
-  }, [editValue, feature.default_value, feature.id, feature.name, setValue]);
+    toast.info(
+      t("labelFeatureUpdated", { ns: "messages", label: feature.name }),
+      TOAST_DISMISS_OPTIONS
+    );
+  }, [editValue, feature.default_value, feature.id, feature.name, setValue, t]);
 
   return (
     <li className="flex max-w-full flex-col items-center justify-between gap-x-6 gap-y-2 py-2 md:flex-row">
@@ -224,7 +237,9 @@ function DropdownFeatureInput(props: DropdownFeatureInputProps) {
             onClick={handleResetToDefault}
           >
             <icons.RotateBackwards className="h-3.5 w-3.5" />
-            <span className="sr-only">Reset</span>
+            <span className="sr-only">
+              {t("buttons.reset", { ns: "labels" })}
+            </span>
           </Button>
           <Button
             type="button"
@@ -234,7 +249,9 @@ function DropdownFeatureInput(props: DropdownFeatureInputProps) {
             onClick={handleSave}
           >
             <icons.Save className="h-3.5 w-3.5" />
-            <span className="sr-only">Save</span>
+            <span className="sr-only">
+              {t("buttons.save", { ns: "labels" })}
+            </span>
           </Button>
         </div>
       </div>
