f618eca2764e5c5ca7e17267133a2658c716cad5
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-02-24 02:38:32 +1300

    fix: the percentage problem

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import { cn } from "@/lib/utils";
+import { calculatePercentage, cn } from "@/lib/utils";
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, style, ...props }, ref) => (
+>(({ className, value, style, max = 100, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +18,9 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{
+        transform: `translateX(-${100 - calculatePercentage(value || 0, max)}%)`,
+      }}
     />
   </ProgressPrimitive.Root>
 ));
