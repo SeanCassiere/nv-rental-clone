2a814d927193e8f9b63db02f2ec50182a4f6f554
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-03-21 20:37:54 +1300

    feat: using a muted-foreground color on the accordion chevron icon

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -40,7 +40,7 @@ const AccordionTrigger = React.forwardRef<
     >
       {children}
       {!hideChevron ? (
-        <icons.ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+        <icons.ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
       ) : null}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
