bf6a28dfea7a94c2dd071971aa66884595ff5418
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-22 20:11:18 +1200

    chore: start using a flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,73 @@
-/** @type {import('eslint').Linter.Config} */
-export default {
-  settings: {
-    react: {
-      createClass: "createReactClass", // Regex for Component Factory to use,
-      pragma: "React",
-      fragment: "Fragment",
-      version: "detect",
+// @ts-check
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tsEslint from "typescript-eslint";
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  {
+    ignores: ["dist/**", "node_modules/**", "src/route-tree.gen.ts"],
+  },
+  {
+    files: ["src/**/*.{ts,tsx}", "index.html", "*.config.js", "*.config.cjs"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/consistent-type-imports": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "jsx-a11y/img-redundant-alt": "off",
     },
   },
-  files: ["src/**/*.{ts,tsx}"],
-  ignorePatterns: ["dist/**", "node_modules/**", "src/route-tree.gen.ts"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-    ecmaFeatures: {
-      jsx: true,
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier
+);
+
+const config = [
+  // {
+  //   ignores: ["./dist/**", "./node_modules/**", "./src/route-tree.gen.ts"],
+  // },
+  {
+    settings: {
+      react: {
+        createClass: "createReactClass", // Regex for Component Factory to use,
+        pragma: "React",
+        fragment: "Fragment",
+        version: "detect",
+      },
     },
+    // files: ["src/**/*.{ts,tsx}"],
+    // parser: "@typescript-eslint/parser",
+    // parserOptions: {
+    //   project: "./tsconfig.json",
+    //   ecmaFeatures: {
+    //     jsx: true,
+    //   },
+    // },
+    languageOptions: {},
+    // plugins: ["@typescript-eslint", "react", "prettier"],
+    // extends: [
+    //   "plugin:react/recommended",
+    //   "plugin:react/jsx-runtime",
+    //   "plugin:react-hooks/recommended",
+    //   "prettier",
+    // ],
+    // rules: {
+    //   "react/react-in-jsx-scope": "off",
+    //   "react/prop-types": "off",
+    //   "react/display-name": "off",
+    //   "react/no-unescaped-entities": "off",
+    //   "react/no-unknown-property": "off",
+    // },
   },
-  plugins: ["@typescript-eslint", "react", "prettier"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-unnecessary-type-constraint": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "jsx-a11y/img-redundant-alt": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "react/no-unescaped-entities": "off",
-    "react/no-unknown-property": "off",
-  },
-};
+];
