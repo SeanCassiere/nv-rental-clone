76bcf1d17ca704f9668e11d87d5c573358b78452
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-30 14:52:09 +1200

    feat: set the toaster position to `bottom-center` and have the background correctly change on color-scheme (#354)
    * feat: toaster position bottom-center
    
    * feat: have the correct toast background colors be applied

--- src/app-entry.tsx
+++ src/app-entry.tsx
@@ -106,11 +106,12 @@ function RouterWithInjectedAuth() {
       <Toaster
         theme={theme.ternaryDarkMode}
         dir={dir}
+        position="bottom-center"
         className="toaster group"
         toastOptions={{
           classNames: {
             toast:
-              "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+              "group toast group-[.toaster]:bg-[var(--toast-background)] group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
             description: "group-[.toast]:text-muted-foreground",
             actionButton:
               "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
