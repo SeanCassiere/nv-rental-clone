e06a2a377c28b0ca42349f0cd63a03c621d38cf1
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-07-14 23:57:47 +1200

    feat: for adding social icons in the future

--- src/routes/-components/auth/footer.tsx
+++ src/routes/-components/auth/footer.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
+import { Link } from "@tanstack/react-router";
 
 import { UI_APPLICATION_NAME } from "@/lib/utils/constants";
 
 export default function AuthFooter() {
   return (
     <footer className="border-t bg-card px-1 md:px-5">
-      <div className="mx-auto flex max-w-[1700px] items-center justify-between px-4 pb-6 pt-5 md:px-10">
-        <div>
-          <p className="text-sm text-foreground/80">
-            © {new Date().getFullYear()} {UI_APPLICATION_NAME}. All rights
-            reserved.
-          </p>
+      <div className="mx-auto grid max-w-[1700px] gap-4 px-4 pb-6 pt-5 md:px-10">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Link to="/">
+              <img
+                // logo url is set in the global css under the name --logo-url
+                className="h-8 w-8 rounded-full p-1 [content:var(--logo-url)]"
+                alt={UI_APPLICATION_NAME}
+                style={{ imageRendering: "crisp-edges" }}
+              />
+            </Link>
+            <p className="text-sm font-medium text-foreground/80">
+              © {new Date().getFullYear()}
+              <span className="sr-only md:not-sr-only">
+                {" "}
+                {UI_APPLICATION_NAME}. All rights reserved.
+              </span>
+            </p>
+          </div>
+          <div>Right</div>
         </div>
-        <div>Right</div>
+        <div>github twitter</div>
       </div>
     </footer>
   );
