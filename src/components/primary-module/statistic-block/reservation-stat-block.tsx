72cd4d203159b0cc1185ca9e4c0e6b138a28e4e5
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-02 16:48:48 +1200

    chore: remaining from "react/react-in-jsx-scope"

--- src/components/primary-module/statistic-block/reservation-stat-block.tsx
+++ src/components/primary-module/statistic-block/reservation-stat-block.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { Link } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 
--- src/lib/types/router.ts
+++ src/lib/types/router.ts
@@ -1,5 +1,3 @@
-import type * as React from "react";
-
 export type LinkComponentProps<TComp> = React.PropsWithoutRef<
   TComp extends React.FC<infer TProps> | React.Component<infer TProps>
     ? TProps
