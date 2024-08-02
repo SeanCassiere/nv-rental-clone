12f6a6b1cfb72b96526da96b33fadccdb497a258
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-02 16:27:14 +1200

    style: rework the eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,43 @@
 // @ts-check
 import eslintJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import reactCompiler from "eslint-plugin-react-compiler";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import eslintReact from "eslint-plugin-react";
+import eslintReactCompiler from "eslint-plugin-react-compiler";
+import eslintReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslintJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
-    ignores: ["dist/**", "node_modules/**", "src/route-tree.gen.ts", "*.html"],
+    files: ["**.config.{cjs,mjs,ts,js}", "postbuild.cjs"],
+    languageOptions: {
+      globals: { ...globals.node },
+      parserOptions: { tsconfigRootDir: import.meta.dirname },
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
   },
   {
-    files: ["src/**/*.{ts,tsx}", "*.config.js", "*.config.cjs"],
-    ...reactRecommended,
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "**.config.{cjs,mjs,ts,js}",
+      "src/**/*.gen.ts",
+      "*.html",
+    ],
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    ...eslintReact.configs.flat.recommended,
+  },
+  {
     languageOptions: {
-      ...reactRecommended.languageOptions,
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
+      ...eslintReact.configs.flat.recommended.languageOptions,
+      globals: { ...globals.browser },
       parserOptions: {
         tsconfigRootDir: import.meta.dirname,
         ecmaFeatures: { jsx: true },
@@ -35,33 +51,33 @@ export default tseslint.config(
         version: "detect",
       },
     },
+  },
+  // {},
+  {
     plugins: {
-      "react-compiler": reactCompiler,
+      "react-compiler": eslintReactCompiler,
       // @ts-expect-error
-      "react-hooks": reactHooks,
-    },
-    // @ts-expect-error
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "no-extra-boolean-cast": "off",
-      "no-case-declarations": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/consistent-type-imports": "off",
-      "@typescript-eslint/no-empty-interface": "off",
-      "@typescript-eslint/no-unnecessary-type-constraint": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "jsx-a11y/img-redundant-alt": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "react/display-name": "off",
-      "react/no-unescaped-entities": "off",
-      "react/no-unknown-property": "off",
-      "react-compiler/react-compiler": "error",
+      "react-hooks": eslintReactHooks,
     },
   },
-  eslintConfigPrettier
+  {
+    rules: {
+      ...eslintReactHooks.configs.recommended.rules,
+      "react-compiler/react-compiler": "error",
+
+      "no-extra-boolean-cast": "off",
+      "no-case-declarations": "off",
+
+      "react/prop-types": "off",
+      "react/display-name": "off",
+
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  }
 );
