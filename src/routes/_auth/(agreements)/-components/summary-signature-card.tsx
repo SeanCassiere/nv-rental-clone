ca5c17c8dae77630e867d806a056ee7084ea649c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-02 22:56:15 +1200

    refactor: cleanup the styling a bit

--- src/routes/_auth/(agreements)/-components/summary-signature-card.tsx
+++ src/routes/_auth/(agreements)/-components/summary-signature-card.tsx
@@ -13,7 +13,6 @@ export default function SummarySignatureCard(props: {
 }) {
   const agreementId = props.agreement.agreementId.toString();
   const drivers = props.agreement.driverList;
-  console.log(props.agreement);
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between border-b px-5 py-4">
@@ -48,40 +47,35 @@ function Driver(props: {
   // const isSigned = !!props.driver.signatureDate;
   const isSigned = !!props.driver.signatureDate;
   return (
-    <li className="flex items-center justify-between gap-2 p-4">
-      <div className="flex justify-start gap-4">
-        <div className="flex items-center justify-center px-1">
-          {isSigned ? (
-            <icons.Check className="size-4" />
-          ) : (
-            <icons.X className="size-4" />
-          )}
-        </div>
-        <div className="min-w-0 flex-auto text-sm">
-          <p className="font-semibold leading-6 text-foreground">
-            {props.driver.driverName}
-          </p>
-          <p className="mt-1 truncate leading-5 text-muted-foreground">
-            <icons.Signature
-              className={cn(
-                "mr-1 inline size-3",
-                isSigned ? "text-green-500" : "text-destructive"
-              )}
-            />
-            {isSigned ? (
-              <>
-                <span className="hidden md:inline">Signed on:</span>
-                <span>&nbsp;{props.driver.signatureDate}</span>
-              </>
-            ) : (
-              <span>Not signed</span>
+    <li className="flex flex-row items-center justify-between gap-4 p-5 xl:gap-2">
+      <div className="grid min-w-0 justify-start text-sm">
+        <p className="inline-flex items-center gap-1 truncate font-semibold leading-6 text-foreground">
+          {props.isPrimary ? <icons.User className="size-3" /> : null}
+          <span>{props.driver.driverName}</span>
+        </p>
+        <p className="mt-1 leading-5 text-muted-foreground">
+          <icons.Signature
+            className={cn(
+              "mr-1 inline size-3",
+              isSigned ? "text-green-500" : "text-destructive"
             )}
-          </p>
-        </div>
+          />
+          {isSigned ? (
+            <>
+              <span className="hidden 2xl:inline">Signed on:</span>
+              <span>&nbsp;{props.driver.signatureDate ?? "..."}</span>
+            </>
+          ) : (
+            <span>Not signed</span>
+          )}
+        </p>
       </div>
       <div className="flex items-center gap-2">
-        <Button variant="outline" size="sm" className="bg-transparent">
-          {isSigned ? "Resign" : "Sign"}
+        <Button variant="outline" size="sm" className="gap-1 bg-transparent">
+          <icons.Signature className="size-3" />
+          <span className="hidden text-xs xl:inline">
+            {isSigned ? "Redo" : "Sign"}
+          </span>
         </Button>
         {isSigned ? (
           <Button variant="ghost" size="icon">
