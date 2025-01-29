e25158a843e399b9dbdd664259d4f9c7a5f60c7f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-01-29 13:49:19 +1300

    chore: switch to a static vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,35 +16,30 @@ const commitHash = cp
 const APP_VERSION = `${packageJson.version}-${commitHash}`;
 
 // Add the ts-expect-error comment since the types for vite-tsconfig-paths are not up to date
-export default defineConfig(({ command }) => {
-  return {
-    server: {
-      port: 3000,
-    },
-    build: {
-      sourcemap: command === "serve",
-    },
-    define: {
-      "import.meta.env.APP_VERSION": JSON.stringify(APP_VERSION),
-    },
-    plugins: [
-      viteTsConfigPaths(),
-      tailwindcss(),
-      TanStackRouterVite({
-        routesDirectory: "./src/routes",
-        generatedRouteTree: "./src/route-tree.gen.ts",
-        routeFileIgnorePrefix: "-",
-        quoteStyle: "double",
-        autoCodeSplitting: true,
-        indexToken: "_index",
-        routeToken: "_route",
-        disableManifestGeneration: true,
-      }),
-      viteReact({
-        babel: {
-          plugins: [["babel-plugin-react-compiler", {}]],
-        },
-      }),
-    ],
-  };
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  define: {
+    "import.meta.env.APP_VERSION": JSON.stringify(APP_VERSION),
+  },
+  plugins: [
+    viteTsConfigPaths(),
+    tailwindcss(),
+    TanStackRouterVite({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/route-tree.gen.ts",
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "double",
+      autoCodeSplitting: true,
+      indexToken: "_index",
+      routeToken: "_route",
+      disableManifestGeneration: true,
+    }),
+    viteReact({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
 });
